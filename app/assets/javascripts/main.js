80c698525143f8d0872250bb8b363aac2966e5e2
Edward Shin <Eshin0910@gmail.com>
2014-02-11 23:16:56 -0500

    graphed gas prices without gridlines

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,5 @@
 var statesArray = [];
+var pricesArray = [];
 $(document).ready(function(){
   console.log('ready');
   getStates();
@@ -15,26 +16,53 @@ function getStates() {
     url: '/states',
     dataType: 'json'
   }).done(function(states) {
-    states.forEach(
-      function pushName(object){
-        statesArray.push(object.name);
-      }
-      );
-    d3.select('body')
+    states.forEach(function(state){
+      pricesArray.push(state.price);
+      statesArray.push(state.name);
+    });
+    //set parameters for svg container
+    var width = 1000;
+    var height = 400;
+
+    //set graph grid on the x & y axis
+    // var xAxis = d3.svg.axis().scale(statesArray)
+    var yAxis = d3.svg.axis().scale(yScale).orient('left');
+
+    //set scaling functions&parameters based on dataset
+    var yScale = d3.scale.linear()
+    .domain([d3.min(pricesArray)-0.1, d3.max(pricesArray)])
+    .range([0, height]);
+
+    var xScale = d3.scale.ordinal()
+    .domain(d3.range(50))
+    .rangeRoundBands([0, width],0.05);
+
+    //append container to body tag
+    var svg = d3.select('body')
     .append('svg')
-    .selectAll('rect')
+    .attr('width', width)
+    .attr('height', height);
+
+    //D3 Magic: selects all rect tags
+    svg.selectAll('rect')
+    //attach data to rect tags
     .data(states)
     .enter()
+    //append rect tags to svg container and set attributes
     .append('rect')
-    .attr('height', 20)
-    .attr('y', function(d,i){
-      return i * 24;
+    .attr('x', function(d,i){
+      return xScale(i);
     })
-    .attr('width', function(d,i){
-      return 100 * d.price;
-    });
-  });
+    .attr('y', function(d, i){
+      return height-(yScale(d.price));
+    })
+    .attr('height', function(d,i){
+      return yScale(d.price);
+    })
+    .attr('width', xScale.rangeBand());
 
-}
+
+  });//---end of done function---
+}//----end of function---
 
 
